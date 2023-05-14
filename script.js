5ea5a0e9972171281602af45be8cd0f0587abd2a
zox2m <zox2mm@gmail.com>
2023-05-15 03:53:41 +0900

    엑셀 업로드는 되는데 내용물이 local에 안드감

--- script.js
+++ script.js
@@ -30,3 +30,42 @@ function getVoca(){
 function removeAllVoca(){
   localStorage.clear();
 }
+
+//엑셀 파일 업로드
+function handleFile(file) {
+  const reader = new FileReader();
+
+  reader.onload = function(e) {
+    const data = new Uint8Array(e.target.result);
+    const workbook = XLSX.read(data, { type: 'array' });
+
+    // Get the first sheet name
+    const sheetName = workbook.SheetNames[0];
+
+    // Get the worksheet
+    const worksheet = workbook.Sheets[sheetName];
+
+    // Convert worksheet to JSON object
+    const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+    // Extract keys and values from JSON data
+    const keys = jsonData.map(row => row[0]);
+    const values = jsonData.map(row => row[1]);
+
+    // Store keys and values in local storage
+    for (let i = 0; i < keys.length; i++) {
+      localStorage.setItem(keys[i], values[i]);
+    }
+
+    // Print stored data to console
+    console.log(localStorage);
+  };
+
+  reader.readAsArrayBuffer(file);
+}
+
+function handleFileInputChange(event) {
+  const file = event.target.files[0];
+  handleFile(file);
+}
+
